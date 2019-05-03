91b548bdbec249ade4e7f558ebbd8ed6ca9291f4
Thomas499 <38442058+Thomas499@users.noreply.github.com>
2019-05-03 06:18:28 -0700

    will compile, need button

--- KotlinYouTubeLBTA/app/src/main/java/com/letsbuildthatapp/kotlinyoutubelbta/MainAdapter.kt
+++ KotlinYouTubeLBTA/app/src/main/java/com/letsbuildthatapp/kotlinyoutubelbta/MainAdapter.kt
@@ -1,5 +1,6 @@
 package com.letsbuildthatapp.kotlinyoutubelbta
 
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.RecyclerView.*
 import android.view.LayoutInflater
@@ -13,24 +14,28 @@ import java.lang.reflect.Constructor
  */
 
 class MainAdapter: Adapter<CustomViewHolder>() {
-
-    val videoTitles = listOf("First title", "Second", "3rd", "MOOOOORE TITLE")
+    //val videoTitles = listOf("First title", "Second", "3rd", "MOOOOORE TITLE")
+    private val name_of_url = arrayListOf<String>(
+            "www.youtube.com" , "www.google.com" , "pagead2.googlesyndication.com" , "nrdp512appboot.netflix.com"
+    )
+    private val blockstatus = arrayListOf<String>(
+            "ok" , "blocked" , "ok" , "blocked"
+    )
+    private val timestamp = arrayListOf<String>(
+            "10:31:04" , "10:31:10" , "10:31:14" , "10:31:16"
+    )
 
     // numberOfItems
     override fun getItemCount(): Int {
-        return videoTitles.size
+        return name_of_url.size
     }
-    //override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CustomViewHolder {
-    // how do we even create a view
-    //    val layoutInflater = LayoutInflater.from(parent?.context)
-    //    val cellForRow = layoutInflater.inflate(R.layout.video_row, parent, false)
-    //   return CustomViewHolder(cellForRow)
-    //}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         // how do we even create a view
         val layoutInflater = LayoutInflater.from(parent.context)
         val cellForRow = layoutInflater.inflate(R.layout.video_row, parent, false)
+
+
         return CustomViewHolder(cellForRow)
     }
 
@@ -39,12 +44,28 @@ class MainAdapter: Adapter<CustomViewHolder>() {
     //   holder?.view?.textView_video_title?.text = videoTitle
     //}
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-        val videoTitle = videoTitles[position]
-        holder.view.textView_video_title?.text = videoTitle
+
+        val timestamp = timestamp[position]
+        val names = name_of_url[position]
+        val blockstatus = blockstatus[position]
+        holder.view.timestamp_textView?.text = timestamp
+        holder.view.name_of_url_textView?.text = names //textView_video_title?.text = names
+        holder.view.blockstatus_textview?.text = blockstatus
     }
 
 }
 
-class CustomViewHolder(val view: View): ViewHolder(view) {
+//class CustomViewHolder(val view: View): ViewHolder(view) {
+
+//}
+// This will make click reload screen. Think I need to add button in here.
+class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+
+    init {
+        view.setOnClickListener {
+            val intent = Intent(view.context, MainActivity::class.java) //CourseDetailActivity::class.java)
+            view.context.startActivity(intent)
+        }
+    }
 
 }
